e7db6e0bea7eb03273091cdaa6396197aad0e5b8
MBogushefsky <mbogushe@asu.edu>
2020-04-25 18:56:40 -0700

    Add prod build

--- src/app/services/location.service.ts
+++ src/app/services/location.service.ts
@@ -11,7 +11,6 @@ export class LocationService {
   getPosition(): Promise<any>
   {
     return new Promise((resolve, reject) => {
-
       navigator.geolocation.getCurrentPosition(resp => {
           resolve({lng: resp.coords.longitude, lat: resp.coords.latitude});
         },
